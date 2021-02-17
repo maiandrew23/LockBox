5bd34722f68e268ecad7fb52eea4c26a0d3d445d
maiandrew23 <Andrew Mai>
2021-02-17 18:20:46 +0000

    Added stub function headers finished #3

--- lockbox.py
+++ lockbox.py
@@ -1,5 +1,40 @@
 def info():  
-    '''Prints a basic library description'''
-    print("Software library for the Lockbox project.")
+  '''Prints a basic library description'''
+  print("Software library for the Lockbox project.")
+
+
+class Lockbox:
+  def __init__(self):
+    #initialize
+  
+  class Solenoid:
+    def __init__(self):
+    
+    def open():
+      return True
+
+    def close():
+      return True
+
+  class Keypad:
+    def __init__(self):
+
+    def readKey():
+      return "String"
+
+  class Display:
+    
+    def __init__(self):
+
+    def on():
+
+    def off():
+    
+  class Printer:
+    def __init__(self):
+      pass
+    def sendText(word):
+      pass
+
 
 
