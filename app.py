e92c36c3e03927b2afc188f40b1fdcb6036c430a
DanielHunte <danieljhunte@hotmail.com>
2021-04-07 13:40:46 -0400

    Changed port number because permission denied, see #13

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def hello():
     return app.send_static_file('html/index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
