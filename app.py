b609e9d8b35fb180668aedbedcceca901b9946c2
DanielHunte <danieljhunte@hotmail.com>
2021-04-07 13:17:59 -0400

    Added one time setups for each component, closes #12

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import lockbox
+import time
+from threading import Thread, Event
 
-def main():
-  print("Application Logic")
-
-main()
+lb = lockbox.Lockbox()
+lb.solenoid.setup()
+lb.display.setup()
+lb.printer.setup()
+lb.keypad.setup()
+print("")
