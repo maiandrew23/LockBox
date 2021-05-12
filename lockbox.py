f809ca65973127e9f79ad5864fc6d2d45551e178
maiandrew23 <55074018+maiandrew23@users.noreply.github.com>
2021-05-12 01:19:03 -0400

    cleaned up lockbox.py

--- lockbox.py
+++ lockbox.py
@@ -1,6 +1,5 @@
 from Adafruit_Thermal import *
 import RPi.GPIO as GPIO
-import sys
 import time
 from threading import Thread, Event
 from RPLCD import CharLCD
@@ -100,90 +99,27 @@ class Keypad:
 class Display:
   def setup(self):
     GPIO.setmode(GPIO.BCM)
-    #GPIO.setup(LCD_RS, GPIO.OUT)
-    #GPIO.setup(LCD_E, GPIO.OUT)
-    #GPIO.setup(LCD_D4, GPIO.OUT)
-    #GPIO.setup(LCD_D5, GPIO.OUT)
-    #GPIO.setup(LCD_D6, GPIO.OUT)
-    #GPIO.setup(LCD_D7, GPIO.OUT)
-
-    #self.write_arr_4bit(LCD_4BIT1, LCD_CMD)
-    #self.write_arr_4bit(LCD_4BIT2, LCD_CMD)
-    #self.write_arr_4bit(LCD_ON_NC, LCD_CMD)
-    #self.write_arr_4bit(LCD_ENTRY, LCD_CMD)
-    #self.write_arr_4bit(LCD_CLEAR, LCD_CMD)
-    print("Display setup finished")
     self.lcd = CharLCD(cols=16,rows=2,pin_rs=6,pin_e=5,pins_data=[13,19,26,1],numbering_mode=GPIO.BCM)
     self.lcd.cursor_mode = 'hide'
-  def on(self):
-    self.write_arr_4bit(LCD_D_ON, LCD_CMD)
-    print("Display is on")
-
-  def off(self):
-    self.write_arr_4bit(LCD_D_OFF, LCD_CMD)
-    print("Display is off")
-  def char_to_arr(self, c):
-    return [int(b) for b in format(ord(c), '08b')]
-
-
-  def write_arr_4bit(self, bits, mode, debug=True):
-
-    pins = [LCD_D7, LCD_D6, LCD_D5, LCD_D4]
-    GPIO.output(LCD_RS, mode)
-    # set the most significant bits (high bits) on data lines
-    for p, b in zip(pins, bits[:4]):
-
-      GPIO.setup(p, GPIO.OUT)
-      GPIO.output(p, b)
-
-    # pulse clock
-    time.sleep(E_DELAY)
-    GPIO.output(LCD_E, GPIO.HIGH)
-    time.sleep(E_PULSE)
-    GPIO.output(LCD_E, GPIO.LOW)
-    time.sleep(E_DELAY)
-
-    #set lease significant bits on data lines
-    for p, b in zip(pins, bits[4:]):
-      GPIO.setup(p, GPIO.OUT)
-      GPIO.output(p,b)
-      # pulse clock
-    time.sleep(E_DELAY)
-    GPIO.output(LCD_E, GPIO.HIGH)
-    time.sleep(E_PULSE)
-    GPIO.output(LCD_E, GPIO.LOW)
-    time.sleep(E_DELAY)
-
-    #reset pins to 0
-    for p in pins:
-      GPIO.output(p,GPIO.LOW)
+    print("Display setup finished")
 
   def clear(self):
-    #self.write_arr_4bit(LCD_CLEAR, LCD_CMD)
     self.lcd.clear()
-    self.lcd.cursor_mode = 'hide'
     return
 
-  def refresh(self):
-      self.lcd.clear()
-      
+
   def show_text(self, text, line=1):
     if line == 1:
       self.lcd.cursor_pos = (0,0)
       self.lcd.write_string("                ")
       self.lcd.cursor_pos = (0,0)
       self.lcd.write_string(text)
-      #self.write_arr_4bit(LCD_LINE_1, LCD_CMD)
+
     elif line == 2:
       self.lcd.cursor_pos = (1,0)
       self.lcd.write_string("                ")
       self.lcd.cursor_pos = (1,0)
       self.lcd.write_string(text)
-      #self.write_arr_4bit(LCD_LINE_2, LCD_CMD)
-    #for c in text:
-      #arr = self.char_to_arr(c)
-      #self.write_arr_4bit(arr, LCD_CHR)
-    #print("'" + text + "'", "on line 1 of display")
 
 class Printer:
   def __init__(self):
